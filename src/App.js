2b8711b53cfde9f19fb11da1f389e4c50519360e
stoombasscode <cesarhuertaescalante@hotmail.com>
2019-06-01 18:53:42 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,69 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { todos } from './todos.json';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { TodoForm } from './components/TodoForm.js'
+
+
+
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      todos: todos
+    }
+
+  }
+  render() {
+
+    const todos = this.state.todos.map((todo, i)=> {
+      return (
+
+        <div className = "col-md-4">
+          <div className = "card mt-4" >
+            <div className = "card-header" >
+                    <h3>  { todo.tittle }</h3>
+                    <span className = "badge badge-pill badge-danger ml-2">
+                          { todo.Priority }
+                    </span>
+                      <div className = "card-body" >
+                              <h3>{todo.description}</h3>
+                              <p><h5>{ todo.responsable }</h5></p>
+                            </div>
+                      </div>
+              </div>
+        </div>
+      )
+    });
+
+    return (
+
+
+
+      <div className="App">
+          <nav className=" navbar navbar-dark bg-dark ">
+            <a href="" className = "text-white">
+            Tareas
+
+              <span className = "badge badge-pill badge-light ml-2">
+                  { this.state.todos.length }
+              </span>
+
+            </a>
+          </nav>
+
+          <div className = "container" >
+            <div className = "row mt-4">
+                  { todos }
+            </div>
+          </div>
+
+
+
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/TodoForm.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+
+
+class TodoForm extends Component {
+  render() {
+    return (
+      <div className="card">
+        <form className="card-body">
+          <div className="form-group">
+            <input
+              type="text"
+              name="title"
+              className="form-control"
+              placeholder="Title"
+              />
+          </div>
+          <div className="form-group">
+            <input
+              type="text"
+              name="responsible"
+              className="form-control"
+              placeholder="Responsible"
+              />
+          </div>
+          <div className="form-group">
+            <input
+              type="text"
+              name="description"
+              className="form-control"
+              placeholder="Description"
+              />
+          </div>
+          <div className="form-group">
+            <select
+                name="priority"
+                className="form-control"
+              >
+              <option>Baja</option>
+              <option>Media</option>
+              <option>Alta</option>
+            </select>
+          </div>
+          <button type="submit" className="btn btn-primary">
+            Guardar
+          </button>
+        </form>
+      </div>
+    )
+  }
+
+}
